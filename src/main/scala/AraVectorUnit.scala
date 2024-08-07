a22966289070f1c87fddde57adddf55057b39868
Jerry Zhao <jerryz123@berkeley.edu>
2024-08-07 15:03:06 -0700

    More tweaks/fixes

--- src/main/scala/AraVectorUnit.scala
+++ src/main/scala/AraVectorUnit.scala
@@ -70,7 +70,7 @@ trait HasLazyAraImpl { this: LazyModuleImp =>
 
   val ara = Module(new AraBlackbox(vLen, nXacts, nLanes, axiIdBits, 64, 1, axiDataWidth))
 
-  val mem_valid = RegNext(ex_valid && !mem_kill, false.B) && !mem_kill
+  val mem_valid = RegNext(ex_valid, false.B) && !mem_kill
   val mem_inst = RegEnable(ex_inst, ex_valid)
   val mem_vconfig = RegEnable(ex_vconfig, ex_valid)
   val mem_rs1 = Mux(mem_inst(14,12).isOneOf(1.U, 5.U) && !mem_inst(6,0).isOneOf(7.U, 39.U),
@@ -326,7 +326,7 @@ class AraRocketUnit(val nLanes: Int, val axiIdBits: Int, val enableDelay: Boolea
     def axiDataWidth = outer.axiDataWidth
     def enableDelay = outer.enableDelay
     status := io.core.status
-    ex_valid := io.core.ex.valid && io.core.ex.ready
+    ex_valid := io.core.ex.valid && io.core.ex.ready && !io.core.wb.replay
     ex_inst := io.core.ex.inst
     ex_pc := io.core.ex.pc
     ex_vconfig := io.core.ex.vconfig
